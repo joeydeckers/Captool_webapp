8d2635cd0ff8088adc8a1fb803fd9038ef35fda7
JoeyDeckers <deckersjoey@gmail.com>
2020-04-19 21:15:12 +0200

    Changes to captool edit

--- src/views/EditCaptionItem.vue
+++ src/views/EditCaptionItem.vue
@@ -5,25 +5,27 @@
         <b-col lg="3">
           <div class="video-item">
             <h2>{{ video.title }}</h2>
-            <iframe
+            <!-- <iframe
               :src="video.link"
               :title="video.title"
               frameborder="0"
               allowfullscreen
-            ></iframe>
+              style="width: 100%; height: 350px;"
+            ></iframe> -->
+            <video controls id="video">
+              <source :src="video.sources[1].file">
+            </video>
           </div>
         </b-col>
-        <!-- <b-col lg="6">
+        <b-col lg="6">
           <b-input placeholder="Jouw caption"></b-input>
           <vue-range-slider
-            :bg-style="bgStyle"
-            :tooltip-style="tooltipStyle"
-            :process-style="processStyle"
+           
             v-model="value"
             :max="max"
           ></vue-range-slider>
           <button>Save</button>
-        </b-col> -->
+        </b-col>
         <b-col lg="3"></b-col>
       </b-row>
     </b-container>
@@ -31,15 +33,15 @@
 </template>
 
 <script>
-// import "vue-range-component/dist/vue-range-slider.css";
-// import VueRangeSlider from "vue-range-component";
+import "vue-range-component/dist/vue-range-slider.css";
+import VueRangeSlider from "vue-range-component";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
   data() {
     return {
-      // value: [0],
-      // max: ""
+      value: [0],
+      max: 0
     };
   },
     methods: {
@@ -50,14 +52,16 @@ export default {
   },
   created() {
     this.fetchVideo(this.$route.params.id);
-    // setTimeout(() => {
-    //   this.value.push(Math.floor(this.video.playlist[0].duration));
-    //   this.max = Math.floor(this.video.playlist[0].duration);
-    // }, 1000);
+    setTimeout(() => {
+       let video = document.getElementById('video');
+       this.value.push(Math.floor(video.duration));
+       this.max = Math.floor(video.duration);
+       console.log(Math.floor(video.duration));
+    }, 1000);
   },
-  // components: {
-  //   VueRangeSlider
-  // },
+  components: {
+    VueRangeSlider
+  },
 };
 </script>
 

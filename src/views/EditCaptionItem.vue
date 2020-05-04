95c66ef49a86a5a7c54d32175043b16410a5fe77
Joey Both <joeyboth@gmail.com>
2020-05-04 16:36:00 +0200

    added srtToJson function

--- src/views/EditCaptionItem.vue
+++ src/views/EditCaptionItem.vue
@@ -19,12 +19,14 @@
           </div>
         </b-col>
         <b-col lg="6">
-          <b-input  v-model="text" placeholder="Jouw caption"></b-input>
+        
           <vue-range-slider
-           
             v-model="value"
             :max="max"
+            :min="min"
           ></vue-range-slider>
+
+            <b-input  v-model="text" placeholder="Jouw caption"></b-input>
           <button @click="addSubtitle()">Save caption</button> <button @click="createSrt()">Save SRT</button>
           <div v-for="subtitle in words" :key="subtitle.id">
             <p>
@@ -43,30 +45,19 @@
 
 <script>
 import "vue-range-component/dist/vue-range-slider.css";
-//import fs from 'fs';
 import VueRangeSlider from "vue-range-component";
 import { mapGetters, mapActions } from "vuex";
 
-const sampleWords = [{
-        "id": 0,
-        "start": 13.02,
-        "end": 13.17,
-        "text": "There"
-      },
-      {
-        "id": 1,
-        "start": 13.17,
-        "end": 13.38,
-        "text": "is"
-      },];
+const sampleWords = [];
+var output = [];
 const formatSeconds = seconds => new Date(seconds.toFixed(3) * 1000).toISOString().substr(11, 12);
+
 
 const srtGenerator = (vttJSON) => {
   let srtOut = '';
   vttJSON.forEach((v, i) => {
     srtOut += `${ i + 1 }\n${ formatSeconds(parseFloat(v.start)).replace('.', ',') } --> ${ formatSeconds(parseFloat(v.end)).replace('.', ',') }\n${ v.text.trim() }\n\n`;
   });
-
   return srtOut;
 };
 
@@ -75,6 +66,7 @@ export default {
     return {
       value: [0],
       max: 0,
+      min: 1,
       words: sampleWords,
       text: ''
     };
@@ -87,13 +79,39 @@ export default {
         let video = document.getElementById('video');
         this.value.push(Math.floor(video.duration));
         this.max = Math.floor(video.duration);
-        console.log(Math.floor(video.duration));
       }, 1000);
     },
     createSrt(){
       const srtData = srtGenerator(sampleWords);
       console.log(srtData);
+      this.srtToJson(srtData);
+    },
+    srtToJson(data){
+      var lines = data.split('\n');
+      var buffer = {
+          content: []
+      };
 
+      lines.forEach(function(line) {
+          if(!buffer.id)
+              buffer.id = line;
+          else if(!buffer.start) {
+              var range = line.split(' --> ');
+              const firstOne = range[0].replace(',000', '');
+              const secondOne = range[1].replace(',000', '');
+              buffer.start = firstOne.split(':').reverse().reduce((prev, curr, i) => prev + curr*Math.pow(60, i), 0);
+              buffer.end =  secondOne.split(':').reverse().reduce((prev, curr, i) => prev + curr*Math.pow(60, i), 0);
+          }
+          else if(line !== '')
+              buffer.text.push(line);
+          else {
+              output.push(buffer);
+              buffer = {
+                  text: []
+              };
+          }
+      })
+      console.log(output);
     },
     addSubtitle(){
       const id = sampleWords.length + 1;

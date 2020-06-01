630986aa1788f7d44b64ab6e23cead8c585bcba9
JoeyDeckers <deckersjoey@gmail.com>
2020-06-01 12:38:15 +0200

    Added test

--- tests/e2e/specs/register.js
+++ tests/e2e/specs/register.js
@@ -5,7 +5,7 @@ describe('Register test', () => {
   it('Can register', () => {
     cy.get('[data-type="email"]').clear().type('testesttest123@gmail.com');
     cy.get('[data-type="name"]').clear().type('Jowi');
-    cy.get('[data-type="password"]').clear().type('testesttest')
+    cy.get('[data-type="password"]').clear().type('testesttest');
 
     cy.get('[type="submit"]').click();
   });

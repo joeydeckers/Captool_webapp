789a42934d2db536a0df43d57be94bcd19672f3e
Jop <jop.huijbers2001@gmail.com>
2020-06-01 15:59:10 +0200

    profile page to api

--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -11,7 +11,7 @@ const getters = {
 };
 
 const actions = {
-    fetchUser({commit}, access_token){
+    fetchUser({ commit }, access_token) {
         axios({
             method: 'get',
             url: 'api/Users/GetUser',
@@ -19,17 +19,42 @@ const actions = {
                 Authorization: "Bearer " + access_token,
             },
         })
-        .then((response)=>{
-            commit('SET_USER', response.data);
-        })
-        .catch((error) => {
-            alert(error);
-        })
+            .then((response) => {
+                commit('SET_USER', response.data);
+            })
+            .catch((error) => {
+                alert(error);
+            })
     },
+    updateUser({ commit }, { access_token, user }) {
+        console.log(access_token);
+        console.log(user);
+        axios({
+            method: 'put',
+            url: 'api/Users/UpdateUser',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                Authorization: "Bearer " + access_token,
+            },
+            data: {
+                name: user.name,
+                email: user.email,
+                playlist: user.playlist,
+                password: user.password
+            }
+        })
+            .then((response) => {
+                commit('SET_USER', response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 };
 
 const mutations = {
-    SET_USER(state, user){
+    SET_USER(state, user) {
         state.user = user;
     }
 };

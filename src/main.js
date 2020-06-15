717a9a7963d6e9ae160654bdefebee0437100cfc
Jop <jop.huijbers2001@gmail.com>
2020-06-15 10:58:09 +0200

    added vuelidate

--- src/main.js
+++ src/main.js
@@ -3,12 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import vuelidate from 'vuelidate'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faEnvelope, faLock, faUser, faPlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.use(vuelidate)
 
 library.add(faEnvelope)
 library.add(faLock)
@@ -22,7 +25,7 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
-axios.defaults.baseURL = 'https://localhost:44320/'; //'https://i346784core.venus.fhict.nl/';
+axios.defaults.baseURL = 'https://i346784core.venus.fhict.nl/'; // 'https://localhost:44320/';
 
 Vue.config.productionTip = false
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,44 +1,51 @@
 <template>
   <div id="login">
     <b-container fluid>
-        <b-row>
-          <b-col lg="6">
-            <img alt="Vue logo" src="../assets/bg.png">
-          </b-col>
-            <b-col lg="6">
-              
-                <b-form @submit.prevent="login">
-                    <h3><strong>Inloggen</strong></h3>
-                    <p>Vul jouw gegevens in om in te loggen.</p>
-                    <b-form-group
-                      id="input-group-1"                     
-                    >
+      <b-row>
+        <b-col lg="6">
+          <img alt="Vue logo" src="../assets/bg.png" />
+        </b-col>
+        <b-col lg="6">
+          <b-form @submit.prevent="login">
+            <h3>
+              <strong>Inloggen</strong>
+            </h3>
+            <p>Vul jouw gegevens in om in te loggen.</p>
+            <b-form-group id="input-group-1">
+              <div class="input-holder">
+                <font-awesome-icon icon="envelope" />
+                <input type="email" placeholder="Jouw email" v-model="email" data-type="email" />
+                <div class="error" v-if="!$v.name.required">Field is required</div>
+                <div
+                  class="error"
+                  v-if="!$v.name.minLength"
+                >Name must have at least {{$v.name.$params.minLength.min}} letters.</div>
+              </div>
+            </b-form-group>
 
-                    <div class="input-holder">
-                      <font-awesome-icon icon="envelope" />
-                      <input type="email" placeholder="Jouw email" v-model="email" data-type="email">
-                    </div>
-                    
-                    </b-form-group>
+            <b-form-group id="input-group-2">
+              <div class="input-holder">
+                <font-awesome-icon icon="lock" />
+                <input
+                  type="password"
+                  placeholder="Jouw wachtwoord"
+                  v-model="password"
+                  data-type="password"
+                />
+              </div>
+            </b-form-group>
 
-                    <b-form-group id="input-group-2">
-                    <div class="input-holder">
-                      <font-awesome-icon icon="lock" />
-                      <input type="password" placeholder="Jouw wachtwoord" v-model="password" data-type="password">
-                    </div>
-                    </b-form-group>
-
-
-                     <Button type="submit" buttonText="Inloggen"/>
-                </b-form>
-            </b-col>
-        </b-row>
+            <Button type="submit" buttonText="Inloggen" />
+          </b-form>
+        </b-col>
+      </b-row>
     </b-container>
   </div>
 </template>
 
 <script>
-import Button from '@/components/Button.vue'
+import Button from "@/components/Button.vue";
+import { required, minLength } from "vuelidate";
 
 export default {
   components: {
@@ -46,13 +53,22 @@ export default {
   },
   data() {
     return {
-      email: '',
-      password: ''
+      email: "",
+      password: ""
     };
   },
   methods: {
     login() {
-      this.$store.dispatch('login', {email: this.email, password: this.password})
+      this.$store.dispatch("login", {
+        email: this.email,
+        password: this.password
+      });
+    }
+  },
+  validations: {
+    name: {
+      required,
+      minLength: minLength(4)
     },
   }
 };

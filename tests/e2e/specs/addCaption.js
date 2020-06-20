f44a528f21c67b8159f40caf5befb3f922075621
JoeyDeckers <deckersjoey@gmail.com>
2020-06-20 14:15:52 +0200

    Busy with tests

---
+++ tests/e2e/specs/addCaption.js
@@ -0,0 +1,13 @@
+describe('Add caption', () => {
+    before(() => {
+      cy.visit('http://localhost:8080/register');
+    });
+    // it('Can add caption', () => {
+    //   cy.get('[data-type="email"]').clear().type('testesttest123@gmail.com');
+    //   cy.get('[data-type="name"]').clear().type('Jowi');
+    //   cy.get('[data-type="password"]').clear().type('testesttest');
+  
+    //   cy.get('[type="submit"]').click();
+    // });
+  })
+  
